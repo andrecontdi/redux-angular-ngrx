83491c3d70b8f4e7d99d677c2a8af16d04514d6a
Andrea <andre.contdi@gmail.com>
2021-09-23 11:53:43 -0300

    feat: basic look and feel

---
+++ redux-app/src/app/counter/grandson/grandson.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grandson',
+  templateUrl: './grandson.component.html',
+  styles: [
+  ]
+})
+export class GrandsonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
