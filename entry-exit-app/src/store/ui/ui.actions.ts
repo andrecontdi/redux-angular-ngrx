6c59e62d0e4def380db21525a595a475f088273d
Andrea <andre.contdi@gmail.com>
2021-10-24 12:58:24 -0300

    feat: addng uistore

---
+++ entry-exit-app/src/store/ui/ui.actions.ts
@@ -0,0 +1,4 @@
+import { createAction } from '@ngrx/store';
+
+export const showLoader = createAction('[UI] Show loader');
+export const hideLoader = createAction('[UI] Hide loader');
---
+++ entry-exit-app/src/store/ui/ui.reducer.ts
@@ -0,0 +1,20 @@
+import { Action, createReducer, on } from "@ngrx/store";
+import { hideLoader, showLoader } from "./ui.actions";
+
+export interface UiState {
+  showLoader: boolean;
+}
+
+export const initialState: UiState = {
+  showLoader: false,
+};
+
+const _uiReducer = createReducer(
+  initialState,
+  on(showLoader, state => {...state, showLoader: true}),
+  on(hideLoader, state => {...state, showLoader: false})
+);
+
+export function uiReducer(state: UiState, action: Action): UiState {
+  return _uiReducer(state, action);
+}
